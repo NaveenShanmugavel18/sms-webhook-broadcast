9fb73bbc3da71eacef98e8f423bea41c2fa3542e
Naveen Shanmugavel <naveensv18@gmail.com>
2018-09-14 01:42:19 +0530

    dynamically added field remove

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,26 +8,22 @@ $( document ).ready( () => {
 		$('.nav li.active').next('li').find('a').attr("data-toggle","tab");
 		$('a[href="'+$('.nav li.active').next('li').find('a').attr('href')+'"]').trigger('click');
 		var inputFields = getInputFields();
+		$('#smsKey option').remove();
 		$.each(inputFields, function(key, value) {
 			$('.input-fields-populate').append($("<option></option>").attr("value",value).text(value)); 
 		});
 	});
-
-	/*$('#configureSms').on('click', () => {
-		$('.nav li.active').next('li').removeClass('disabled');
-		$('.nav li.active').next('li').find('a').attr("data-toggle","tab");
-		$('a[href="'+$('.nav li.active').next('li').find('a').attr('href')+'"]').trigger('click');
-		var inputFields = getInputFields();
-		$.each(inputFields, function(key, value) {
-			$('#smsKey').append($("<option></option>").attr("value",value).text(value)); 
-		});
-	});*/
 
 	$('#addField').on('click', () => {
 		$('.input-fields').append('<div class="row"><div class="form-group col-md-5">\
 		<input type="text" name="inputkeys[]" class="form-control" placeholder="Key"/></div>\
 		<div class="form-group col-md-5"><input type="text" name="inputvalues[]" class="form-control" placeholder="Text / Number"/></div>\
 		<a href="#" class="remove_field"><i class="glyphicon glyphicon-trash"></i></a></div>');
+	});
+
+	$('body').on('click', '.remove_field', (e) => {
+		var $this = $(e.currentTarget);
+		$this.closest('div.row').remove();
 	});
 
 	$('#clipboardCopy').on('click', () => {
